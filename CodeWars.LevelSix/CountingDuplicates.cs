05a2c6dfd977f1313f61878691cf08308d77d934
Mattias Hedlund <mattias.hedlund@sogeti.com>
2022-12-16 14:40:52 +0100

    save

--- CodeWars.LevelSix/CountingDuplicates.cs
+++ CodeWars.LevelSix/CountingDuplicates.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 namespace CodeWars.LevelSix;
 
 public class CountingDuplicates
-{
+{   
   public static int DuplicateCount(string str)
   {    
-    return str.ToLower().GroupBy(x => x).Where(x => x.Count() > 1).Count();
+    return str.ToLower().GroupBy(c => c).Where(g => g.Count() > 1).Count();
+  }
+
+  public static int DuplicateCountV2(string str)
+  {
+    return str.ToLower().GroupBy(c => c).Count(c => c.Count() > 1);
   }
 }
