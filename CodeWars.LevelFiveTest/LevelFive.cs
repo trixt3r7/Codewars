81fa8210c5fa0cd672956f49a16c8f0ec3ff3c9c
Mattias Hedlund <mattias.hedlund@sogeti.com>
2023-01-20 14:13:24 +0100

    2 solutions

--- CodeWars.LevelFiveTest/LevelFive.cs
+++ CodeWars.LevelFiveTest/LevelFive.cs
@@ -83,4 +83,60 @@ namespace CodeWars.LevelFiveTest
       Assert.That(WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"), Is.EqualTo("11 11 2000 10003 22 123 1234000 44444444 9999"));
     }
   }
+
+  [TestFixture]
+  public class ProdFibTests
+  {
+
+    [Test]
+    public void Test0()
+    {
+      ulong[] r = new ulong[] { 3, 5, 1 };
+      Assert.That(ProdFib.ProductFib(15), Is.EqualTo(r));
+    }
+    [Test]
+    public void Test1()
+    {
+      ulong[] r = new ulong[] { 55, 89, 1 };
+      Assert.That(ProdFib.ProductFib(4895), Is.EqualTo(r));
+    }
+    [Test]
+    public void Test2()
+    {
+      ulong[] r = new ulong[] { 21, 34, 1 };
+      Assert.That(ProdFib.ProductFib(714), Is.EqualTo(r));
+    }
+    [Test]
+    public void Test3()
+    {
+      ulong[] r = new ulong[] { 34, 55, 0 };
+      Assert.That(ProdFib.ProductFib(800), Is.EqualTo(r));
+    }
+  }
+
+  [TestFixture]
+  public static class ScrambliesTests
+  {
+
+    private static void testing(bool actual, bool expected)
+    {
+      Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public static void ScrambleTest1()
+    {
+      testing(Scramblies.Scramble("rkqodlw", "woorld"), false);
+      testing(Scramblies.Scramble("rkqodlw", "world"), true);
+      testing(Scramblies.Scramble("cedewaraaossoqqyt", "codewars"), true);
+      testing(Scramblies.Scramble("katas", "steak"), false);
+      testing(Scramblies.Scramble("scriptjavx", "javascript"), false);
+      testing(Scramblies.Scramble("scriptingjava", "javascript"), true);
+      testing(Scramblies.Scramble("scriptsjava", "javascripts"), true);
+      testing(Scramblies.Scramble("javscripts", "javascript"), false);
+      testing(Scramblies.Scramble("aabbcamaomsccdd", "commas"), true);
+      testing(Scramblies.Scramble("commas", "commas"), true);
+      testing(Scramblies.Scramble("sammoc", "commas"), true);
+    }
+  }
 }
