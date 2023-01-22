62d2365c10fce80647d3b6f359bf5a31119e9d65
trixt3r7 <matteh78@outlook.com>
2023-01-22 17:58:15 +0100

    1 solution

---
+++ CodeWars.LevelFive/PagnationHelper.cs
@@ -0,0 +1,75 @@
+﻿using System.Reflection.Metadata;
+
+namespace CodeWars.LevelFive;
+
+public class PagnationHelper<T>
+{
+  private readonly IList<T> collection;
+  private readonly int itemsPerPage;
+
+  // TODO: Complete this class
+
+  /// <summary>
+  /// Constructor, takes in a list of items and the number of items that fit within a single page
+  /// </summary>
+  /// <param name="collection">A list of items</param>
+  /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+  public PagnationHelper(IList<T> collection, int itemsPerPage)
+  {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+  }
+
+  /// <summary>
+  /// The number of items within the collection
+  /// </summary>
+  public int ItemCount
+  {
+    get
+    {
+      return collection.Count();
+    }
+  }
+
+  /// <summary>
+  /// The number of pages
+  /// </summary>
+  public int PageCount
+  {
+    get
+    {
+      return ItemCount / itemsPerPage + 1;
+    }
+  }
+
+  /// <summary>
+  /// Returns the number of items in the page at the given page index 
+  /// </summary>
+  /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+  /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+  public int PageItemCount(int pageIndex)
+  {
+    return (pageIndex  < 0 || pageIndex >= PageCount) 
+      ? -1 
+      : collection.Skip(pageIndex * itemsPerPage).Take(itemsPerPage).Count();
+  }
+
+  /// <summary>
+  /// Returns the page index of the page containing the item at the given item index.
+  /// </summary>
+  /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+  /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+  public int PageIndex(int itemIndex)
+  {
+    return (itemIndex < 0 || itemIndex >= this.ItemCount)
+      ? -1
+      : itemIndex / itemsPerPage;
+
+    //return itemIndex >= 0 && itemIndex < ItemCount
+    //  ? collection.Chunk(itemsPerPage)
+    //                    .Select(page => page.Contains(collection[itemIndex]))
+    //                    .TakeWhile(i => i == false)
+    //                    .Count()
+    //  : -1;
+  }
+}
--- CodeWars.LevelFiveTest/LevelFive.cs
+++ CodeWars.LevelFiveTest/LevelFive.cs
@@ -264,4 +264,67 @@ namespace CodeWars.LevelFiveTest
       testing(Scramblies.Scramble("sammoc", "commas"), true);
     }
   }
+
+  // TODO: Replace examples and use TDD development by writing your own tests
+
+  [TestFixture]
+  public class PagnationTest
+  {
+    private readonly IList<int> collection = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
+    private PagnationHelper<int> helper;
+
+    [SetUp]
+    public void SetUp()
+    {
+      helper = new PagnationHelper<int>(collection, 10);
+    }
+
+    [Test]
+    [TestCase(-1, ExpectedResult = -1)]
+    [TestCase(0, ExpectedResult = 10)]
+    [TestCase(1, ExpectedResult = 10)]
+    [TestCase(2, ExpectedResult = 4)]
+    [TestCase(3, ExpectedResult = -1)]
+    public int PageItemCountTest(int pageIndex)
+    {
+      return helper.PageItemCount(pageIndex);
+    }
+
+    [Test]
+    [TestCase(-1, ExpectedResult = -1)]
+    [TestCase(0, ExpectedResult = 0)]
+    [TestCase(1, ExpectedResult = 0)]
+    [TestCase(9, ExpectedResult = 0)]
+    [TestCase(10, ExpectedResult = 1)]
+    [TestCase(11, ExpectedResult = 1)]
+    [TestCase(19, ExpectedResult = 1)]
+    [TestCase(20, ExpectedResult = 2)]
+    [TestCase(21, ExpectedResult = 2)]
+    [TestCase(22, ExpectedResult = 2)]
+    [TestCase(23, ExpectedResult = 2)]
+    [TestCase(24, ExpectedResult = -1)]
+    public int PageIndexTest(int itemIndex)
+    {
+      return helper.PageIndex(itemIndex);
+    }
+
+    [Test]
+    public void ItemCountTest()
+    {
+      Assert.That(helper.ItemCount, Is.EqualTo(24));
+    }
+
+    [Test]
+    public void PageCountTest()
+    {
+      Assert.That(helper.PageCount, Is.EqualTo(3));
+    }
+    
+    [Test]
+    public void EmptyCollectionPageIndexTest()
+    {
+      helper = new PagnationHelper<int>(new List<int>(), 10);
+      Assert.That(helper.PageIndex(0), Is.EqualTo(-1));
+    }
+  }
 }
