c75a095622ab69bc0d8dd211a414025c1f822e35
Mattias Hedlund <mattias.hedlund@sogeti.com>
2023-04-21 15:58:14 +0200

    new HighScoringWord

---
+++ CodeWars.LevelSix/HighestScoringWord.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeWars.LevelSix
+{
+  public class HighestScoringWord
+  {
+    public static string High(string s)
+    {
+      var words = s.Split(" ");
+      List<int> scores = s.Split(" ").Select(word => word.Sum(x => x - 96)).ToList();      
+      return words[scores.IndexOf(scores.Max())];
+    }
+
+    public static string HighV2(string s)
+    {
+      return s.Split(' ').OrderByDescending(x => x.Sum(z => z - 'a' + 1)).First();
+    }
+
+    public static string HighV1(string s)
+    {
+      var words = s.Split(" ");
+      var scores = new List<int>();
+      foreach (var item in words)
+      {
+        scores.Add(item.Sum(x => x - 96));
+      }
+      var max = scores.Max();
+      var index = scores.IndexOf(max);
+      return words[index];
+    }
+  }
+}
--- CodeWars.LevelSixTest/LevelSIx.cs
+++ CodeWars.LevelSixTest/LevelSIx.cs
@@ -441,4 +441,26 @@ namespace CodeWars.LevelSixTest
       return FindUniqueNumber.GetUnique(numbers);
     }
   }
+
+  [TestFixture]
+  public class HighestScoringWordTests
+  {
+    private static IEnumerable<TestCaseData> testCases
+    {
+      get
+      {
+        yield return new TestCaseData("man i need a taxi up to ubud").Returns("taxi");
+        yield return new TestCaseData("what time are we climbing up to the volcano").Returns("volcano");
+        yield return new TestCaseData("take me to semynak").Returns("semynak");
+        yield return new TestCaseData("aa b").Returns("aa");
+        yield return new TestCaseData("b aa").Returns("b");
+        yield return new TestCaseData("bb d").Returns("bb");
+        yield return new TestCaseData("d bb").Returns("d");
+        yield return new TestCaseData("aaa b").Returns("aaa");
+      }
+    }
+
+    [Test, TestCaseSource("testCases")]
+    public string Test(string s) => HighestScoringWord.High(s);
+  }
 }
