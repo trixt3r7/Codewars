9dd91e7eeb08592c7c1a7bfe7bb5d66a28849dd8
Mattias Hedlund <mattias.hedlund@sogeti.com>
2023-04-26 21:43:32 +0200

    One 4kyu Solution

---
+++ CodeWars.LevelFour/SumOfIntervals.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.LevelFour
+{
+  public class SumOfIntervals
+  {
+    public static int SumIntervals((int, int)[] intervals)
+    {
+      var prevMax = int.MinValue;
+
+      return intervals
+          .OrderBy(x => x.Item1)
+          .ThenBy(x => x.Item2)
+          .Aggregate(0, (acc, x) => acc += prevMax < x.Item2 ? -Math.Max(x.Item1, prevMax) + (prevMax = x.Item2) : 0);
+    }
+
+    public static int SumIntervalsV3((int, int)[] intervals)
+    {
+      if (intervals.Count() == 0)
+      {
+        return 0;
+      }
+
+      var intervalsSorted = intervals.OrderBy(t => t.Item1).ThenBy(t => t.Item2).ToList();
+
+
+
+      int sumIntervals = 0;
+      int i = 0;
+      int j = 0;
+      while (i < intervalsSorted.Count)
+      {
+        sumIntervals += intervalsSorted[i].Item2 - intervalsSorted[i].Item1;
+
+        j = i + 1;
+
+        while (j < intervalsSorted.Count
+          && intervalsSorted[i].Item1 <= intervalsSorted[j].Item1
+          && intervalsSorted[i].Item2 >= intervalsSorted[j].Item2)
+        {
+          j++;
+        }
+        if (j < intervalsSorted.Count && (intervalsSorted[i].Item2 > intervalsSorted[j].Item1))
+        {
+          sumIntervals -= intervalsSorted[i].Item2 - intervalsSorted[j].Item1;
+        }
+
+        i = j;
+      }
+
+      //var intervalsFiltered = intervalsSorted.Skip(1)
+      //  .Where((value, index) => !(value.Item1 >= intervalsSorted[index].Item1 && value.Item2 <= intervalsSorted[index].Item2))
+      //  .Prepend(intervalsSorted[0]).ToList();     
+
+      // Subtract overlapping value from Sum
+      //sumIntervals -= intervalsFiltered.Skip(1)
+      //  .Select((value, index) => (intervalsFiltered[index].Item2 > value.Item1) ? intervalsFiltered[index].Item2 - value.Item1 : 0).Sum();
+
+      return sumIntervals;
+    }
+
+    public static int SumIntervalsV2((int, int)[] intervals)
+    {
+      if (intervals.Count() == 0)
+      {
+        return 0;
+      }
+
+      var intervalsSorted = intervals.OrderBy(t => t.Item1).ThenBy(t => t.Item2).ToList();      
+      int sumIntervals = 0;
+      int i = 0;
+      int j = 0;      
+      while (i < intervalsSorted.Count)
+      {
+        sumIntervals += intervalsSorted[i].Item2 - intervalsSorted[i].Item1;
+        
+        j = i + 1;
+        while (j < intervalsSorted.Count 
+          && intervalsSorted[i].Item1 <= intervalsSorted[j].Item1 
+          && intervalsSorted[i].Item2 >= intervalsSorted[j].Item2)
+        {
+          j++;
+        }
+        if(j < intervalsSorted.Count && (intervalsSorted[i].Item2 > intervalsSorted[j].Item1))
+        {
+          sumIntervals -= intervalsSorted[i].Item2 - intervalsSorted[j].Item1;
+        }        
+        i = j;
+      }
+      return sumIntervals;
+    }
+
+    public static int SumIntervalsV1((int, int)[] intervals)
+    {
+      if (intervals.Count() == 0)
+      {
+        return 0;
+      }
+
+      // Sort the intervals
+      var intervalsSorted = intervals.OrderBy(t => t.Item1).ThenBy(t => t.Item2).ToList();
+      List<(int, int)> intervalsFiltered = new();
+
+      int i = 0;
+      int j = 0;
+      while (i < intervalsSorted.Count)
+      {
+        intervalsFiltered.Add(intervalsSorted[i]);
+
+        j = i + 1;
+        while (j < intervalsSorted.Count 
+          && intervalsSorted[i].Item1 <= intervalsSorted[j].Item1 
+          && intervalsSorted[i].Item2 >= intervalsSorted[j].Item2)
+        {
+          j++;
+        }
+        i = j;        
+      }
+
+      int sumIntervals = intervalsFiltered.Sum(t => t.Item2 - t.Item1);     
+      sumIntervals -= intervalsFiltered.Skip(1)
+        .Select((value, index) => (intervalsFiltered[index].Item2 > value.Item1) ? intervalsFiltered[index].Item2 - value.Item1 : 0).Sum();
+
+      return sumIntervals;
+    }
+  }
+}
--- CodeWars.LevelFourTest/LevelFour.cs
+++ CodeWars.LevelFourTest/LevelFour.cs
@@ -1,4 +1,6 @@
 using CodeWars.LevelFour;
+using NUnit.Framework.Constraints;
+using Interval = System.ValueTuple<int, int>; // For SumOfIntervalTest
 
 namespace CodeWars.LevelFourTest
 {
@@ -153,4 +155,65 @@ namespace CodeWars.LevelFourTest
 
 
     }
+
+  public class SumOfIntervalTest
+  {
+    [Test]
+    public void ShouldHandleEmptyIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { }), Is.EqualTo(0));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (4, 4), (6, 6), (8, 8) }), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ShouldAddDisjoinedIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (1, 2), (6, 10), (11, 15) }), Is.EqualTo(9));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (4, 8), (9, 10), (15, 21) }), Is.EqualTo(11));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (-1, 4), (-5, -3) }), Is.EqualTo(7));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (-245, -218), (-194, -179), (-155, -119) }), Is.EqualTo(78));
+    }
+
+    [Test]
+    public void ShouldHandleNegativeIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (-332_642_732, -326_840_777), (-496_175_526, -488_321_420), (-284, -276), (11, 15) }), Is.EqualTo(13_656_073));     
+    }
+
+    [Test]
+    public void ShouldAddAdjacentIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (1, 2), (2, 6), (6, 55) }), Is.EqualTo(54));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (-2, -1), (-1, 0), (0, 21) }), Is.EqualTo(23));
+    }
+
+    [Test]
+    public void ShouldAddOverlappingIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (1, 4), (7, 10), (3, 5) }), Is.EqualTo(7));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (5, 8), (3, 6), (1, 2) }), Is.EqualTo(6));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (1, 5), (10, 20), (1, 6), (16, 19), (5, 11) }), Is.EqualTo(19));
+    }
+
+    [Test]
+    public void ShouldHandleMixedIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (2, 5), (-1, 2), (-40, -35), (6, 8) }), Is.EqualTo(13));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (-7, 8), (-2, 10), (5, 15), (2000, 3150), (-5400, -5338) }), Is.EqualTo(1234));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (-101, 24), (-35, 27), (27, 53), (-105, 20), (-36, 26) }), Is.EqualTo(158));
+    }
+
+    [Test]
+    public void ShouldHandleLargeIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (-1_000_000_000, 1_000_000_000) }), Is.EqualTo(2_000_000_000));
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (0, 20), (-100_000_000, 10), (30, 40) }), Is.EqualTo(100_000_030));
+    }
+
+    [Test]
+    public void InsaneIntervals()
+    {
+      Assert.That(SumOfIntervals.SumIntervals(new Interval[] { (83, 99), (406, 425), (443, 454), (-396, -381), (-381, -375), (257, 276), (246, 262), (-460, -446), (-324, -314), (74, 77), (-317, -302), (-241, -235), (442, 461), (446, 455), (-222, -204), (411, 418), (152, 166), (182, 188), (321, 323) }), Is.EqualTo(190));
+    }
+  }
 }
